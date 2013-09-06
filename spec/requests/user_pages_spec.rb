3256fe9629f87683d97f44e2045954869de4155c
byelipk <byelipk@gmail.com>
2013-09-06 16:19:26 -0300

    Finishing signup form 2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,12 +27,21 @@ describe "signup" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+      
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end 
     end
 
     describe "with valid information" do
+      
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
@@ -43,6 +52,15 @@ describe "signup" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving a user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email("user@example.com") }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
   end
 end
